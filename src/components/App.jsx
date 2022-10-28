6147de81590630c545a7da94b27823e24ab9c18e
Artem <104884717+ArtemTeslenko@users.noreply.github.com>
2022-10-28 14:46:15 +0300

    Moved main logic into App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,73 @@
-import Feedback from './Feedback/Feedback';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import feedbackOptions from '../Utils/feedbackOptions.json';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+import { StatisticsTitle } from './Statistics/Statistics.styled';
 
-export const App = () => {
-  return (
-    <div className="mainContainer">
-      <Feedback feedbackOptions={feedbackOptions} />
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = e => {
+    const target = e.target.name;
+    this.setState(previous => ({ [target]: previous[target] + 1 }));
+  };
+
+  countTotalFeedback = () =>
+    this.state.good + this.state.neutral + this.state.bad;
+
+  countPositiveFeedbackPercentage = () => {
+    const check = this.countTotalFeedback();
+    if (check) {
+      return Math.floor((this.state.good / this.countTotalFeedback()) * 100);
+    } else {
+      return 0;
+    }
+  };
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positive = this.countPositiveFeedbackPercentage();
+    const { good, neutral, bad } = this.state;
+    return (
+      <div className="mainContainer">
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={feedbackOptions}
+            onLeaveFeedback={this.handleIncrement}
+          ></FeedbackOptions>
+          <StatisticsTitle>Statistics</StatisticsTitle>
+          {total ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positive}
+            ></Statistics>
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+App.propTypes = {
+  test: PropTypes.string,
+  feedbackOptions: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    })
+  ),
 };
